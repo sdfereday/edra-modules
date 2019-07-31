72977bd55a5aa16d138e4994a41511b7dbb4d9e1
Sam <sdfereday@gmail.com>
2019-07-31 21:55:18 +0100

    Adding toolbox and a bunch of other stuff that isnt interesting

---
+++ Modules/Toolbox/Assets/Scripts/Toolbox/Helpers/VectorHelpers.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace RedPanda.Toolbox.Helpers
+{
+    public static class Vectors
+    {
+        public static float Dist(Vector2 a, Vector2 b) => Vector2.Distance(a, b);
+    }
+}
