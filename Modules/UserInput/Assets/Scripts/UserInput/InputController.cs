f47abf7f76a8eb19adf799e222ecc7bb7d39b9bb
Sam <sdfereday@gmail.com>
2019-08-01 10:37:55 +0100

    Pushing user input

---
+++ Modules/UserInput/Assets/Scripts/UserInput/InputController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+namespace RedPanda.UserInput
+{
+    // See: https://bitbucket.org/drunkenoodle/rr-clone/src for original (and battle ideas).
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class InputController : MonoBehaviour, IDirectionInfo
+    {
+        public delegate void InputAction(INPUT_TYPE inputType);
+        public static event InputAction OnInput;
+
+        public float maxSpeed = 5f;
+        public bool InteractionsEnabled { get; private set; }
+        public bool MovementEnabled { get; private set; }
+        public Vector2 Facing { get; set; }
+        public Vector2 CurrentVelocity => rbody.velocity;
+
+        private Rigidbody2D rbody;
+
+        private void OnSceneLoadComplete() => ToggleMovement(true);
+        private void OnSceneLoadStarted() => ToggleMovement(false);
+
+        private void Start()
+        {
+            rbody = GetComponent<Rigidbody2D>();
+            rbody.gravityScale = 0;
+            rbody.freezeRotation = true;
+
+            Facing = Vector2.right;
+
+            InteractionsEnabled = OnInput != null;
+            MovementEnabled = true;
+        }
+
+        private void Update()
+        {
+            rbody.velocity = Vector2.zero;
+
+            if (InteractionsEnabled)
+            {
+                if (Input.GetKeyDown(KeyCodeConsts.USE))
+                {
+                    OnInput(INPUT_TYPE.USE);
+                }
+
+                if (Input.GetKeyDown(KeyCodeConsts.CANCEL))
+                {
+                    OnInput(INPUT_TYPE.CANCEL);
+                }
+            }
+
+            if (MovementEnabled)
+            {
+                float xAxis = Input.GetAxisRaw(KeyCodeConsts.Horizontal);
+                float yAxis = Input.GetAxisRaw(KeyCodeConsts.Vertical);
+
+                if (xAxis != 0 || yAxis != 0)
+                {
+                    Vector2 nm = new Vector2(xAxis * maxSpeed, yAxis * maxSpeed).normalized;
+                    rbody.velocity = nm * maxSpeed;
+                    Facing = CurrentVelocity.normalized;
+                }
+            }
+        }
+
+        public void ToggleInteractions(bool state) => InteractionsEnabled = state;
+
+        public void ToggleMovement(bool state) => MovementEnabled = state;
+
+        public Vector2 GetFirectionVector2D() => Facing;
+
+        public float GetCurrentMagnitude() => CurrentVelocity.magnitude;
+    }
+}
